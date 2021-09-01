08ae529af52035e3ba796b32744ea9737ebbbf6c
Stephen Gowen <sjgowen@gmail.com>
2021-09-01 19:03:50 +0100

    Failing Computing_Magnitude_of_Vector_1_0_0.

---
+++ RayTracer_Tests/MagnitudeNormalisationEtc.cs
@@ -0,0 +1,15 @@
+﻿using RayTracer;
+using Xunit;
+
+namespace RayTracer_Tests;
+public class MagnitudeNormalisationEtc
+{
+    [Fact]
+    public void Computing_Magnitude_of_Vector_1_0_0()
+    {
+        RayVector vector = new(1, 0, 0);
+        var actual = Magnitude(vector);
+        var expected = 1;
+        Assert.Equal(expected, actual);
+    }
+}
