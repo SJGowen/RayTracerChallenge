484e535171fa848ce36038fd55d3046ef08135c1
Stephen Gowen <sjgowen@gmail.com>
2021-09-01 14:06:43 +0100

    Failing Test Adding two Tuples.

---
+++ RayTracer_Tests/PointsAndVectorsMaths.cs
@@ -0,0 +1,16 @@
+﻿using RayTracer;
+using Xunit;
+
+namespace RayTracer_Tests;
+public class PointsAndVectorsMaths
+{
+    [Fact]
+    public void Adding_two_Tuples()
+    {
+        RayTuple tuple1 = new(3, -2, 5, 1);
+        RayTuple tuple2 = new(-2, 3, 1, 0);
+        RayTuple actual = tuple1 + tuple2;
+        RayTuple expected = new(1, 1, 6, 1);
+        Assert.Equal(expected, actual);
+    }
+}
