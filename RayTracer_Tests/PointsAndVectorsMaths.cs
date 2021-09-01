f2311a65f229fe51efe118fbf67328c9d30c2bb0
Stephen Gowen <sjgowen@gmail.com>
2021-09-01 17:08:26 +0100

    Test Subtracting_Vector_from_Point doesn't need any additional code.

--- RayTracer_Tests/PointsAndVectorsMaths.cs
+++ RayTracer_Tests/PointsAndVectorsMaths.cs
@@ -9,7 +9,7 @@ public class PointsAndVectorsMaths
     {
         RayTuple tuple1 = new(3, -2, 5, 1);
         RayTuple tuple2 = new(-2, 3, 1, 0);
-        RayTuple actual = tuple1 + tuple2;
+        var actual = tuple1 + tuple2;
         RayTuple expected = new(1, 1, 6, 1);
         Assert.True(expected.IsEqual(actual));
         Assert.True(expected == actual);
@@ -21,8 +21,18 @@ public class PointsAndVectorsMaths
     {
         RayPoint point1 = new(3, 2, 1);
         RayPoint point2 = new(5, 6, 7);
-        RayTuple actual = point1 - point2;
+        var actual = point1 - point2;
         RayVector expected = new(-2, -4, -6);
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Subtracting_Vector_from_Point()
+    {
+        RayPoint point1 = new(3, 2, 1);
+        RayVector vector2 = new(5, 6, 7);
+        var actual = point1 - vector2;
+        RayPoint expected = new(-2, -4, -6);
         Assert.Equal(expected, actual);
     }
 }
