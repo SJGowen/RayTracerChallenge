afef71b8e6c0bf39543a39411cce4ee66d6d7b25
Stephen Gowen <sjgowen@gmail.com>
2021-09-01 19:23:41 +0100

    Test Computing_Magnitude_of_Vector_0_0_1 added and passing, no additional code needed.

--- RayTracer_Tests/MagnitudeNormalisationEtc.cs
+++ RayTracer_Tests/MagnitudeNormalisationEtc.cs
@@ -16,7 +16,16 @@ public class MagnitudeNormalisationEtc
     [Fact]
     public void Computing_Magnitude_of_Vector_0_1_0()
     {
-        RayVector vector = new(1, 0, 0);
+        RayVector vector = new(0, 1, 0);
+        var actual = RayVector.Magnitude(vector);
+        var expected = 1;
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Computing_Magnitude_of_Vector_0_0_1()
+    {
+        RayVector vector = new(0, 0, 1);
         var actual = RayVector.Magnitude(vector);
         var expected = 1;
         Assert.Equal(expected, actual);
