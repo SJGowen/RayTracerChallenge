c1b817d47371d4cecb95bcc6b555f247438ac201
Stephen Gowen <sjgowen@gmail.com>
2021-09-01 12:29:56 +0100

    Failing test class RayTuple does not exist.

--- RayTracer_Tests/PointsAndVectors.cs
+++ RayTracer_Tests/PointsAndVectors.cs
@@ -4,8 +4,14 @@ namespace RayTracer_Tests;
 public class PointsAndVectors
 {
     [Fact]
-    public void Test1()
+    public void A_Tuple_with_W_equal_1_is_a_Point()
     {
-
+        RayTuple tuple = new(4.3, -4.2, 3.1, 1.0);
+        Assert.Equal(4.3, tuple.X);
+        Assert.Equal(-4.2, tuple.Y);
+        Assert.Equal(3.1, tuple.Z);
+        Assert.Equal(1.0, tuple.W);
+        Assert.True(tuple.IsPoint);
+        Assert.False(tuple.IsVector);
     }
 }
