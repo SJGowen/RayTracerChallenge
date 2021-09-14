00d2506bb87e60033d0cb60e5c7a412fc72a92a2
Stephen Gowen <sjgowen@gmail.com>
2021-09-14 11:54:29 +0100

    Renamed the ConstructingAndInspecting to ConstructAndInspect.

--- RayTracerTests/MatrixTests.cs
+++ RayTracerTests/MatrixTests.cs
@@ -6,7 +6,7 @@ namespace RayTracerTests;
 public class MatrixTests
 {
     [Fact]
-    public void ConstructingAndInspecting4x4Matrix()
+    public void ConstructAndInspect4x4Matrix()
     {
         Matrix matrix = new(4, 4);
         matrix.Add(1, 2, 3, 4, 5.5, 6.5, 7.5, 8.5, 9, 10, 11, 12, 13.5, 14.5, 15.5, 16.5);
