49ff00ecfb83b5a210f1d8b4aae06ebf484b321e
Stephen Gowen <sjgowen@gmail.com>
2021-09-14 11:50:33 +0100

    Added a passing test, ConstructAndInspect4x4Matrix.

---
+++ RayTracer/Matrix.cs
@@ -0,0 +1,18 @@
+﻿namespace RayTracer;
+public class Matrix
+{
+    public double[,] Cells { get; }
+
+    public Matrix(int x, int y)
+    {
+        Cells = new double[x, y];
+    }
+
+    public void Add(params double[] values)
+    {
+        for (int x = 0; x < Cells.GetLength(0); x++)
+            for (int y = 0; y < Cells.GetLength(1); y++)
+                Cells[x, y] = values[x * Cells.GetLength(0) + y];
+    }
+}
+
---
+++ RayTracerTests/MatrixTests.cs
@@ -0,0 +1,22 @@
+﻿using RayTracer;
+using Xunit;
+
+namespace RayTracerTests;
+
+public class MatrixTests
+{
+    [Fact]
+    public void ConstructingAndInspecting4x4Matrix()
+    {
+        Matrix matrix = new(4, 4);
+        matrix.Add(1, 2, 3, 4, 5.5, 6.5, 7.5, 8.5, 9, 10, 11, 12, 13.5, 14.5, 15.5, 16.5);
+        Assert.Equal(1, matrix.Cells[0, 0]);
+        Assert.Equal(4, matrix.Cells[0, 3]);
+        Assert.Equal(5.5, matrix.Cells[1, 0]);
+        Assert.Equal(7.5, matrix.Cells[1, 2]);
+        Assert.Equal(11, matrix.Cells[2, 2]);
+        Assert.Equal(13.5, matrix.Cells[3, 0]);
+        Assert.Equal(15.5, matrix.Cells[3, 2]);
+    }
+}
+
