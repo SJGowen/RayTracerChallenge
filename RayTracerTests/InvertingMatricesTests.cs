9e8dd5aab59d621d239de8eb6c593ecfbdaf73de
Stephen Gowen <sjgowen@gmail.com>
2021-10-11 17:01:13 +0100

    Passing test SubmatrixOf4x4MatrixIs3x3Matrix added.

--- RayTracerTests/InvertingMatricesTests.cs
+++ RayTracerTests/InvertingMatricesTests.cs
@@ -22,4 +22,13 @@ public class InvertingMatricesTests
         Matrix expected = new(-3, 2, 0, 6);
         Assert.Equal(expected, actual);
     }
+
+    [Fact]
+    public void SubmatrixOf4x4MatrixIs3x3Matrix()
+    {
+        Matrix matrix = new(-6, 1, 1, 6, -8, 5, 8, 6, -1, 0, 8, 2, -7, 1, -1, 1);
+        var actual = Matrix.SubMatrix(matrix, 2, 1);
+        Matrix expected = new(-6, 1, 6, -8, 8, 6, -7, -1, 1);
+        Assert.Equal(expected, actual);
+    }
 }
