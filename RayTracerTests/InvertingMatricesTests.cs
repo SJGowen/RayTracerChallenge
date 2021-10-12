56b2fd1a9de3bcff7af88090862ca96640547d50
Stephen Gowen <sjgowen@gmail.com>
2021-10-12 14:46:39 +0100

    Passing test TestingANonInvertibleMatrixForInvertibility added.

--- RayTracerTests/InvertingMatricesTests.cs
+++ RayTracerTests/InvertingMatricesTests.cs
@@ -12,4 +12,11 @@ public class InvertingMatricesTests
         Matrix matrix = new(6, 4, 4, 4, 5, 5, 7, 6, 4, -9, 3, -7, 9, 1, 7, -6);
         Assert.NotEqual(0, Matrix.Determinant(matrix));
     }
+
+    [Fact]
+    public void TestingANonInvertibleMatrixForInvertibility()
+    {
+        Matrix matrix = new(-4, 2, -2, -3, 9, 6, 2, 6, 0, -5, 1, -5, 0, 0, 0, 0);
+        Assert.Equal(0, Matrix.Determinant(matrix));
+    }
 }
