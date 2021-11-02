397284dfa2406bb9169249ad89b35d1380be9ec4
Stephen Gowen <sjgowen@gmail.com>
2021-11-02 14:59:21 +0000

    Passing test ScalingMatrixAppliedToPoint added.

---
+++ RayTracer/Scaling.cs
@@ -0,0 +1,11 @@
+﻿namespace RayTracer;
+
+public class Scaling : RayTuple
+{
+    public Scaling(double x, double y, double z) : base(x, y, z, 0) { }
+
+    public static RayPoint operator *(Scaling transform, RayPoint point)
+    {
+        return new RayPoint(transform.X * point.X, transform.Y * point.Y, transform.Z * point.Z);
+    }
+}
