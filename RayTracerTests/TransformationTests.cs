5d4b8444f78eaf1069088f25bbf808e08caf2ff1
Stephen Gowen <sjgowen@gmail.com>
2021-11-10 17:22:05 +0000

    Passing test MultiplyingByInverseTranslationMatrix added.

--- RayTracerTests/TransformationTests.cs
+++ RayTracerTests/TransformationTests.cs
@@ -12,4 +12,13 @@ public class TransformationTests
         RayPoint point = new(-3, 4, 5);
         Assert.Equal(new RayPoint(2, 1, 7), transform * point);
     }
+
+    [Fact]
+    public void MultiplyingByInverseTranslationMatrix()
+    {
+        Matrix transform = Matrix.Translation(5, -3, 2);
+        Matrix inverse = Matrix.Inverse(transform);
+        RayPoint point = new(-3, 4, 5);
+        Assert.Equal(new RayPoint(-8, 7, 3), inverse * point);
+    }
 }
