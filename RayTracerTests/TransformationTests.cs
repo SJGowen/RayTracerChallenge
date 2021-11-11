d3653dbd051d42b169f7cc1b30e35ffa152bc1d3
Stephen Gowen <sjgowen@gmail.com>
2021-11-11 08:11:55 +0000

    Passing test AShearingTransformationMovesZInProportionToY added.

--- RayTracerTests/TransformationTests.cs
+++ RayTracerTests/TransformationTests.cs
@@ -142,4 +142,12 @@ public class TransformationTests
         RayPoint point = new(2, 3, 4);
         Assert.Equal(new RayPoint(2, 3, 6), transform * point);
     }
+
+    [Fact]
+    public void AShearingTransformationMovesZInProportionToY()
+    {
+        Matrix transform = Matrix.Shearing(0, 0, 0, 0, 0, 1);
+        RayPoint point = new(2, 3, 4);
+        Assert.Equal(new RayPoint(2, 3, 7), transform * point);
+    }
 }
